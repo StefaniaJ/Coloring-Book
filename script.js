d185a62ee8173cff55983146f4484cd7c1b910f3
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-10-29 22:23:07 +0100

    fetch the svg img, added buttons html, change the color

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+let buttons = document.querySelectorAll("button");
+var colr = "white";
+
+window.addEventListener("DOMContentLoaded", loadSVG);
+
+async function loadSVG() {
+  let svgData = await fetch("p.svg");
+  let svgFile = await svgData.text();
+  document.querySelector("#theSVG").innerHTML = svgFile;
+
+  registerEvent();
+}
+
+function registerEvent() {
+  // document.querySelector("#Circle").addEventListener("click", clickCircle);
+  document.querySelector("#Shapes").addEventListener("click", clickShapes);
+}
+
+// function clickCircle() {
+//   console.log("circle clicked");
+// }
+
+function clickShapes(event) {
+  const target = event.target;
+  console.log("clicked: ");
+  console.log(target);
+  console.log("Color:" + target.style.fill);
+  target.style.fill = colr;
+}
+buttons.forEach(setClr);
+function setClr(btn) {
+  btn.addEventListener("click", function() {
+    colr = this.id;
+    // if (btn.id == "reset") {
+    //   location.reload();
+    // }
+  });
+  btn.style.backgroundColor = btn.id;
+}
