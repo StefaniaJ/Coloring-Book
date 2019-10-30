0dab9a1388d89a2fa5f0ce2c0bf95c3c2ae277e4
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-10-30 08:22:17 +0100

    select color-console

--- script.js
+++ script.js
@@ -4,23 +4,39 @@ var colr = "white";
 
 window.addEventListener("DOMContentLoaded", loadSVG);
 
+//Fetch the character
 async function loadSVG() {
   let svgData = await fetch("sp.svg");
   let svgFile = await svgData.text();
   document.querySelector("#theSVG").innerHTML = svgFile;
-
+  //call the event listener for character
   registerEvent();
+  loadBtns();
 }
 
+//Fetch the buttons
+async function loadBtns() {
+  let btnsData = await fetch("colors.svg");
+  let btnsFile = await btnsData.text();
+  document.querySelector("#theColors").innerHTML = btnsFile;
+  //call the event listener for buttons
+  registerColors();
+}
+
+//add an event listener for every shapes of character
 function registerEvent() {
-  // document.querySelector("#Circle").addEventListener("click", clickCircle);
   document.querySelector("#Shapes").addEventListener("click", clickShapes);
+  // document
+  //   .querySelector("#colors-palette")
+  //   .addEventListener("click", clickColors);
 }
 
-// function clickCircle() {
-//   console.log("circle clicked");
-// }
+function registerColors() {
+  document.querySelector("#Shapes").addEventListener("click", clickShapes);
+  document.querySelector("#Colors").addEventListener("click", clickColors);
+}
 
+//When you click a shape, add color white
 function clickShapes(event) {
   const target = event.target;
   console.log("clicked: ");
@@ -28,13 +44,19 @@ function clickShapes(event) {
   console.log("Color:" + target.style.fill);
   target.style.fill = colr;
 }
-buttons.forEach(setClr);
-function setClr(btn) {
-  btn.addEventListener("click", function() {
-    colr = this.id;
-    if (btn.id == "reset") {
-      location.reload();
-    }
-  });
-  btn.style.backgroundColor = btn.id;
+
+function clickColors(event) {
+  // buttons.forEach(setClr);
+  // function setClr(btn) {
+  //   btn.addEventListener("click", function() {
+  //     colr = this.id;
+  //     if (btn.id == "reset") {
+  //       location.reload();
+  //     }
+  //   });
+  //   btn.style.backgroundColor = btn.id;
+  //}
+  const target = event.target;
+  console.log("color: ");
+  console.log(target);
 }
